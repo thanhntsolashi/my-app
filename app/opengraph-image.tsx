570c8e216682c204eec0c9fc41aa9e13a5e28614
thanhnt <thanhnt@solashi.com>
2026-07-08 09:31:43 +0700

    fix deploy

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from "next/og";
 
-// Image metadata
 export const size = {
   width: 1200,
   height: 630,
@@ -8,10 +7,8 @@ export const size = {
 
 export const contentType = "image/png";
 
-// Image generation
-export default async function Image() {
+export default function Image() {
   return new ImageResponse(
-    // ImageResponse JSX element
     <div
       style={{
         fontSize: 128,
